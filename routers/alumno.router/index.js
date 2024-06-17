7f465d76e1dd6970f675464eb48ee03c5a2aec08
idasso <idasso@fi.uba.ar>
2024-06-18 00:18:50 +0200

    Backend con datos de demo

--- routers/alumno.router/index.js
+++ routers/alumno.router/index.js
@@ -1,5 +1,19 @@
 const register = (router) => {
-  router.get("/status", (req, resp) => resp.json({ status: 200 }));
+  router.get("/status", (req, resp) =>{
+    
+    const deviceData = {
+      dispositivoId: 23,
+      nombre: "Demo-ESP32-C3",
+      ubicacion: "Francia",
+      luz1: 0,
+      luz2: 0,
+      temperatura: 38,
+      humedad: 28
+    };
+    resp.json(deviceData);
+    resp.send.status(200);
+    
+  });
 
   //listar leds de la db
   router.get("/leds", (req, resp) => resp.json({ status: 200 }));
