a3c7c5a16f651dff73f2006a6e815b7841b9c130
Ubuntu <ubuntu@ip-172-31-33-4.eu-north-1.compute.internal>
2024-06-19 00:29:21 +0000

    Prueba de conexión con Frontend - falla por problema de CORS

--- routers/alumno.router/index.js
+++ routers/alumno.router/index.js
@@ -1,6 +1,19 @@
 const register = (router) => {
-  router.get("/status", (req, resp) => resp.json({ status: 200 }));
-
+  //router.get("/status", (req, resp) => resp.json({ status: 200 }));
+	router.get('/status', (req, res) => {
+  	// Acá vamos a obtener los datos de temperatura y humedad desde una base de datos.
+  	// Por simplicidad, para la prueba vamos a usar valores de ejemplo.
+  		const deviceData = {
+    			dispositivoId: 23,
+    			nombre: "Demo-ESP32-C3",
+    			ubicacion: "Francia",
+    			luz1: 0,
+    			luz2: 0,
+    			temperatura: 38,
+    			humedad: 28
+  		};
+  		res.json(deviceData);
+	});
   //listar leds de la db
   router.get("/leds", (req, resp) => resp.json({ status: 200 }));
   return router;
